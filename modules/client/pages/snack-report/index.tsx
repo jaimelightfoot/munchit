0a34bc78ce1f7926e9a3c526a5c980e0958400e4
Jaime Lightfoot <jaime.c.lightfoot@gmail.com>
2017-10-26 11:31:18 -0400

    Step 2.1

---
+++ modules/client/pages/snack-report/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { SnackReportUI } from "client/pages/snack-report/snack-report-ui";
+
+export const SnackReportPage: React.SFC = props => {
+  return <SnackReportUI rows={[]} />;
+};
